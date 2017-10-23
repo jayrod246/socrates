11cc68bdedb61618293b8ae6fd5deda055899dd3
Jayrod246 <meyerja.2013@gmail.com>
2017-10-23 17:41:20 -0400

    Renamed ChunkIdentifier to ChunkIdentity to avoid confusion with IChunkIdentifier.

---
+++ Meyer.Socrates/Data/ChunkIdentity.cs
@@ -0,0 +1,25 @@
+﻿namespace Meyer.Socrates.Data
+{
+    public struct ChunkIdentity
+    {
+        public ChunkIdentity(Quad quad, uint id) : this()
+        {
+            Quad = quad;
+            ID = id;
+        }
+
+        public uint ID { get; }
+        public Quad Quad { get; }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode() ^ Quad.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ChunkIdentity other) return ID == other.ID && Quad == other.Quad;
+            return base.Equals(obj);
+        }
+    }
+}
