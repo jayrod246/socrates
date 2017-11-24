63b922a70b48d8a859655ee2fbd8762df39d6856
Jayrod246 <meyerja.2013@gmail.com>
2017-11-24 13:41:54 -0500

    Updated TXXF to use matrix2x3 instead of vectors.
    Naively, I overlooked the fact that TXXF uses a 2x3 transformation matrix. This matrix is now exposed through the property TransformMatrix. The Position and Scale properties were updated to use values from the matrix, and a new Rotation property has been added.

--- Meyer.Socrates/Data/BrMatrix2x3.cs
+++ Meyer.Socrates/Data/BrMatrix2x3.cs
@@ -130,6 +130,52 @@
             }
         }
 
+        public void TRS(out BrVector2 translation, out BrAngle rotation, out BrVector2 scale)
+        {
+            var matrix = this;
+            translation = matrix.GetRow(2);
+            matrix.SetRow(2, new BrVector2());
+            scale = new BrVector2(BrVector.GetMagnitude(matrix.GetRow(0)), BrVector.GetMagnitude(matrix.GetRow(1)));
+            for (int row = 0;row < 2;row++)
+            {
+                for (int col = 0;col < 2;col++)
+                {
+                    var sc = scale[row];
+                    if (sc == 0)
+                        matrix[col, row] = 0f;
+                    else matrix[col, row] /= sc;
+                }
+            }
+
+            rotation = BrAngle.FromRadians((-(float)Math.Asin(matrix[1, 0]) +
+                                             (float)Math.Acos(matrix[1, 1]) +
+                                             (float)Math.Acos(matrix[0, 0]) +
+                                             (float)Math.Asin(matrix[0, 1])) / 4f);
+
+        }
+
+        public static BrMatrix2x3 FromTRS(BrVector2 translation, BrAngle rotation, BrVector2 scale)
+        {
+            var mt = new BrMatrix2x3(
+                1, 0,
+                0, 1,
+                translation.X, translation.Y);
+
+            var c = (BrScalar)Math.Cos(rotation.AsSingle(true));
+            var s = (BrScalar)Math.Sin(rotation.AsSingle(true));
+            var mr = new BrMatrix2x3(
+                c, -s,
+                s, c,
+                0, 0);
+
+            var ms = new BrMatrix2x3(
+                scale.X, 0,
+                0, scale.Y,
+                0, 0);
+
+            return ms * mr * mt;
+        }
+
         public static BrMatrix2x3 operator *(BrMatrix2x3 a, BrMatrix2x3 b)
         {
             return Multiply(a, b);
--- Meyer.Socrates/Data/Sections/TXXF.cs
+++ Meyer.Socrates/Data/Sections/TXXF.cs
@@ -2,48 +2,99 @@
 {
     using Meyer.Socrates.Data;
     using Meyer.Socrates.IO;
-    using System;
     using System.IO;
 
     [SectionKey("TXXF")]
     public sealed class TXXF: VirtualSection
     {
         #region Constructors
-        public TXXF() : this(new BrVector2(), new BrVector2(1, 1))
+        public TXXF() : this(new BrVector2(), new BrVector2(1, 1), BrAngle.MinValue)
         { }
 
-        public TXXF(BrVector2 position, BrVector2 scale)
+        public TXXF(BrVector2 position, BrVector2 scale) : this(position, scale, BrAngle.MinValue)
+        { }
+
+        public TXXF(BrVector2 position, BrVector2 scale, BrAngle rotation)
         {
+            TransformMatrix = BrMatrix2x3.Identity;
             Position = position;
             Scale = scale;
+            Rotation = rotation;
         }
         #endregion
 
-        public BrVector2 Position { get => GetValue<BrVector2>(); set => SetValue(value); }
-        public BrVector2 Scale { get => GetValue<BrVector2>(); set => SetValue(value); }
-        public int Unk1 { get => GetValue<int>(); set => SetValue(value); }
-        public int Unk2 { get => GetValue<int>(); set => SetValue(value); }
+        public BrVector2 Position
+        {
+            get => GetValue<BrVector2>();
+            set
+            {
+                if (SetValue(value))
+                {
+                    transformMatrix = null;
+                }
+            }
+        }
+
+        public BrAngle Rotation
+        {
+            get => GetValue<BrAngle>();
+            set
+            {
+                if (SetValue(value))
+                {
+                    transformMatrix = null;
+                }
+            }
+        }
+
+        public BrVector2 Scale
+        {
+            get => GetValue<BrVector2>();
+            set
+            {
+                if (SetValue(value))
+                {
+                    transformMatrix = null;
+                }
+            }
+        }
+
+        public BrMatrix2x3 TransformMatrix
+        {
+            get
+            {
+                using (Lock())
+                {
+                    return (transformMatrix ?? (transformMatrix = BrMatrix2x3.FromTRS(Position, Rotation, Scale))).Value;
+                }
+            }
+
+            set
+            {
+                using (Lock())
+                {
+                    value.TRS(out var t, out var r, out var s);
+                    Position = t;
+                    Rotation = r;
+                    Scale = s;
+                    transformMatrix = value;
+                }
+            }
+        }
 
         protected override void Read(IDataReadContext c)
         {
             if (c.Length != 28) throw new InvalidDataException("TXXF section length must be 28.");
-            MagicNumber = c.AssertAny(Ms3dmm.MAGIC_NUM_US, Ms3dmm.MAGIC_NUM_JP);
-            var scale = new BrVector2() { X = c.Read<BrScalar>() };
-            Unk1 = c.Read<Int32>();
-            Unk2 = c.Read<Int32>();
-            scale.Y = c.Read<BrScalar>();
-            Scale = scale;
-            Position = c.Read<BrVector2>();
+            MagicNumber = c.Read<uint>();
+            TransformMatrix = c.Read<BrMatrix2x3>();
         }
 
         protected override void Write(IDataWriteContext c)
         {
             c.Write(MagicNumber);
-            c.Write(Scale.X);
-            c.Write(Unk1);
-            c.Write(Unk2);
-            c.Write(Scale.Y);
-            c.Write(Position);
+            c.Write(TransformMatrix);
         }
+
+        private BrMatrix2x3? transformMatrix;
     }
 }
